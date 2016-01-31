cee79f0bcdf1d3f8e855bf59581a1f9a9c923f21
David <dbworth@gmail.com>
2016-01-31 16:52:06 +1000

    Add new test script sound_sensor_test.py

---
+++ nxt/nxt_python/sensor_tests/sound_sensor_test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+# Test the LEGO Mindstorms NXT sound sensor
+
+import roslib; roslib.load_manifest("nxt_python")
+import rospy
+import time
+import nxt.locator
+from nxt.sensor import *
+
+def test_sensors(b):
+        rospy.init_node('test_sensor')
+
+        ss = Sound(b, PORT_1)
+        print "This is the sound sensor test. Make sure that your sound sensor is plugged into Port 1:"
+
+        ss.set_adjusted(active=False) # un-adjusted
+        start = rospy.Time.now()
+        while rospy.Time.now() < start+rospy.Duration(5.0):
+          print 'UN-ADJUSTED SOUND READING:', ss.get_loudness() # get the scaled value
+          time.sleep(0.1)
+
+        ss.set_adjusted(active=True) # adjusted
+        start = rospy.Time.now()
+        while rospy.Time.now() < start+rospy.Duration(5.0):
+          print 'ADJUSTED SOUND READING:', ss.get_loudness() # get the scaled value
+          time.sleep(0.1)
+
+
+b = nxt.locator.find_one_brick()
+
+test_sensors(b)
+
