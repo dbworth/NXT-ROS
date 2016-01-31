7eccb1900d9cf5735e2f33463a74159c59c04722
David <dbworth@gmail.com>
2016-01-31 16:02:24 +1000

    Modify touch_sensor_test.py for NXT-Python v2.2.2

--- nxt/nxt_python/sensor_tests/touch_sensor_test.py
+++ nxt/nxt_python/sensor_tests/touch_sensor_test.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+# Test the LEGO Mindstorms NXT touch sensor
+
 import roslib; roslib.load_manifest("nxt_python")
 import rospy
 import time
@@ -9,18 +11,16 @@ from nxt.sensor import *
 def test_sensors(b):
         rospy.init_node('test_sensor')
 
-        t = TouchSensor(b, PORT_1)
+        t = Touch(b, PORT_1)
         print "This is the touch sensor test. Make sure that your touch sensor is plugged into port 1:"
         print "TOUCH READING:"
-        start =rospy.Time.now()
-        while rospy.Time.now()<start+rospy.Duration(5.0):
+        start = rospy.Time.now()
+        while rospy.Time.now() < start+rospy.Duration(5.0):
           print 'TOUCH:', t.get_sample()
           time.sleep(0.1)
 
 
-sock = nxt.locator.find_one_brick()
-if sock:
-	test_sensors(sock.connect())
-	sock.close()
-else:
-	print 'No NXT bricks found'
+b = nxt.locator.find_one_brick()
+
+test_sensors(b)
+
