7a050030d289882b0e14bdb5c8935a782a856553
David <dbworth@gmail.com>
2016-01-31 16:21:10 +1000

    Modify color_sensor_test.py for NXT-Python v2.2.2

--- nxt/nxt_python/sensor_tests/color_sensor_test.py
+++ nxt/nxt_python/sensor_tests/color_sensor_test.py
@@ -10,38 +10,46 @@ from nxt.sensor import *
 
 def test_sensors(b):
         rospy.init_node('test_sensor')
-        cs = ColorSensor(b, PORT_1)
+
+        cs = Color20(b, PORT_1)
         print "This is the color sensor test make sure that your color sensor is plugged into port 1:"
+
 	print 'RED:'
-        cs.set_light_color('red')
+        cs.set_light_color(Type.COLORRED)
         time.sleep(1.0)
+
 	print 'BLUE:'
-        cs.set_light_color('blue')
+        cs.set_light_color(Type.COLORBLUE)
         time.sleep(1.0)
+
 	print 'GREEN:'
-        cs.set_light_color('green')
+        cs.set_light_color(Type.COLORGREEN)
         time.sleep(1.0)
+
 	print 'WHITE:'
-        cs.set_light_color('white')
+        cs.set_light_color(Type.COLORFULL)
         time.sleep(1.0)
+
 	print 'OFF:'
-        cs.set_light_color(None)
+        cs.set_light_color(Type.COLORNONE)
+        time.sleep(0.2)
+
         print "INTENSITY READING:"
-        start =rospy.Time.now()
-        while rospy.Time.now()<start+rospy.Duration(5.0):
-          print 'INTENSITY:', cs.get_reflected_light('blue')
+        start = rospy.Time.now()
+        while rospy.Time.now() < start+rospy.Duration(5.0):
+          print 'INTENSITY:', cs.get_reflected_light(Type.COLORBLUE)
           time.sleep(0.1)
+
         print "COLOR READING:"
-        start =rospy.Time.now()
-        while rospy.Time.now()<start+rospy.Duration(5.0):
-          print 'COLOR:', cs.get_color()
+        start = rospy.Time.now()
+        while rospy.Time.now() < start+rospy.Duration(5.0):
+          print 'COLOR:', cs.get_color() # this uses Type.COLORFULL
           time.sleep(0.1)
 
+        cs.set_light_color(Type.COLORNONE)
 
 
-sock = nxt.locator.find_one_brick()
-if sock:
-	test_sensors(sock.connect())
-	sock.close()
-else:
-	print 'No NXT bricks found'
+b = nxt.locator.find_one_brick()
+
+test_sensors(b)
+
